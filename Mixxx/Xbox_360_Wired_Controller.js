a81d8fa747aaf894c5cf45ec11b0149352f44e95
deltakurshiva <deltakurshiva@gmail.com>
2013-06-15 23:19:11 +0200

    finished up this controller setting. could maybe use more polish but it's fully featured now at least.

--- Mixxx/Xbox_360_Wired_Controller.js
+++ Mixxx/Xbox_360_Wired_Controller.js
@@ -7,7 +7,9 @@
 function Xbox360Controller() {
     this.controller = new HIDController();
     this.controller.activeDeck = 1;
-
+    this.controller.toggleButtons.push("flanger");
+    //this.controller.toggleButtons.push("talkover");
+    
     this.registerInputPackets = function() {
         packet = new HIDPacket("control", [0x0, 0x14], 14);
 
@@ -28,29 +30,44 @@ function Xbox360Controller() {
         packet.addControl("hid", "left_stick_click", 2, "B", 0x40);
         packet.addControl("hid", "right_stick_click", 2, "B", 0x80);
 
-        // 0x0 left, 0xff right
-        packet.addControl("hid","left_stick_x",0x6,"B");
+        // Toggle trigger sensitivity
+        packet.addControl("hid","left_trigger",4,"B");
+        packet.addControl("hid","right_trigger",5,"B");
+
+        // sticks
+        packet.addControl("hid","left_stick_x",0x6,"h");
         packet.setMinDelta("hid","left_stick_x",2);
-        packet.addControl("hid","right_stick_x",0x8,"B");
-        packet.setMinDelta("hid","right_stick_x",2);
-
-        // 0x0 top, 0xff bottom
-        packet.addControl("hid","left_stick_y",0x7,"B");
+        packet.addControl("hid","left_stick_y",0x8,"h");
         packet.setMinDelta("hid","left_stick_y",2);
-        packet.addControl("hid","right_stick_y",0x9,"B");
+        
+        packet.addControl("hid","right_stick_x",0xA,"h");
+        packet.setMinDelta("hid","right_stick_x",2);
+        packet.addControl("hid","right_stick_y",0xC,"h");
         packet.setMinDelta("hid","right_stick_y",2);
-
-        // Toggle button pressure sensitivity 0x00-0xff
-        packet.addControl("hid","left_trigger",0x12,"B");
-        packet.setMinDelta("hid","left_trigger",4);
-        packet.addControl("hid","right_trigger",0x13,"B");
-        packet.setMinDelta("hid","right_trigger",4);
 
         this.controller.registerInputPacket(packet);
     }
+    
+    this.controller.LEDStates = { off: 0x0,
+                                  blink_all: 0x01, 
+                                  flash_on_1: 0x02,
+                                  flash_on_2: 0x03,
+                                  flash_on_3: 0x04,
+                                  flash_on_4: 0x05,
+                                  on_1: 0x06,
+                                  on_2: 0x07,
+                                  on_3: 0x08,
+                                  on_4: 0x09,
+                                  rotate: 0x0a,
+                                  blink_prev: 0x0b,
+                                  slow_blink_prev: 0x0c,
+                                  alternate: 0x0d };
 
-    // Sixxaxis has no output controls
-    this.registerOutputPackets = function() { }
+    this.registerOutputPackets = function() {
+        packet = new HIDPacket('controller_led',[0x01,0x03],3);    
+        packet.addOutput("hid","led_ring",2,"B");
+        this.controller.registerOutputPacket(packet);
+    }
 
     // No default scalers: all controls done with callbacks anyway
     this.registerScalers = function() { }
@@ -60,7 +77,6 @@ function Xbox360Controller() {
 
 }
 
-// SonySixxAxisController is defined in SonySixxAxis.hid.js
 Xbox360 = new Xbox360Controller();
 
 // Mandatory function for mixxx controllers
@@ -69,6 +85,7 @@ Xbox360.init = function(id) {
     var controller = Xbox360.controller;
     Xbox360.registerInputPackets();
     Xbox360.registerCallbacks();
+    Xbox360.registerOutputPackets();
 
     controller.startAutoRepeatTimer = function(timer_id,interval) {
         if (controller.timers[timer_id])
@@ -78,12 +95,32 @@ Xbox360.init = function(id) {
             "Xbox360.controller.autorepeatTimer()"
         )
     }
+    engine.connectControl("[Master]", "crossfader", "Xbox360.resolve_xfade");    
     HIDDebug("Xbox 360 controller initialized: " + Xbox360.id);
 }
 
+Xbox360.resolve_xfade = function(value, group, control) {
+    var controller = Xbox360.controller;
+    print(value);
+    if (value == 0) {
+        Xbox360.changeLED(controller.LEDStates['off']);
+    } else if (value == 1) {
+        Xbox360.changeLED(controller.LEDStates['on_2']);
+    } else if (value == -1) {
+        Xbox360.changeLED(controller.LEDStates['on_1']);
+    } else if (value > 0) {
+        Xbox360.changeLED(controller.LEDStates['on_4']);
+    } else {
+        Xbox360.changeLED(controller.LEDStates['on_3']);
+    }
+}
+
+
 // Mandatory function for mixxx controllers
 Xbox360.shutdown = function() {
-    Xbox360.controller.close();
+    var controller = Xbox360.controller;
+    engine.connectControl("[Master]", "crossfader", "Xbox360.resolve_xfade", true);
+    controller.close();
     HIDDebug("Xbox 360 controller shutdown: " + Xbox360.id);
 }
 
@@ -101,7 +138,7 @@ Xbox360.registerCallbacks = function(id) {
         return;
     }
     if (controller==undefined) {
-        HIDDebug("Error registrering callbacks: controller is undefined");
+        HIDDebug("Error registering callbacks: controller is undefined");
         return;
     }
 
@@ -122,150 +159,90 @@ Xbox360.registerCallbacks = function(id) {
     controller.linkControl("hid","left_stick_click","deck1","eject");
     controller.linkControl("hid","right_stick_click","deck2","eject");
 
-    // Callbacks for toggle buttons front of controller
-    controller.setCallback("control","hid","button_bottom_left",Xbox360.front_left);
-    controller.setCallback("control","hid","button_top_left",Xbox360.front_left);
-    controller.setCallback("control","hid","button_bottom_right",Xbox360.front_right);
-    controller.setCallback("control","hid","button_top_right",Xbox360.front_right);
+    // Callbacks for triggers
+    controller.setCallback("control","hid","left_trigger",Xbox360.left_volume);
+    controller.setCallback("control","hid","right_trigger",Xbox360.right_volume);
 
-    // Callbacks for pressure sensitive buttons front of controller
-    controller.setCallback("control","hid","pressure_bottom_left",Xbox360.left_bend);
-    controller.setCallback("control","hid","pressure_top_left",Xbox360.left_bend);
-    controller.setCallback("control","hid","pressure_bottom_right",Xbox360.right_bend);
-    controller.setCallback("control","hid","pressure_top_right",Xbox360.right_bend);
+    // Callbacks for sticks
+    controller.setCallback("control","hid","left_stick_x",Xbox360.left_shuttle);
+    controller.setCallback("control","hid","left_stick_y",Xbox360.left_jog);
+    controller.setCallback("control","hid","right_stick_x",Xbox360.right_shuttle);
+    controller.setCallback("control","hid","right_stick_y",Xbox360.right_jog);
 
-    // Callbacks for jog controls
-    controller.setCallback("control","hid","jog_left_y",Xbox360.left_jog);
-    controller.setCallback("control","hid","jog_right_y",Xbox360.right_jog);
-
-    controller.setCallback("control","hid","jog_left_x",Xbox360.left_jog);
-    controller.setCallback("control","hid","jog_right_x",Xbox360.right_jog);
-
-};
-
-Xbox360.front_left = function(field) {
-    var controller = Xbox360.controller;
-    if (field.value==controller.buttonStates.released)
-        return;
-    if (!controller.modifiers.get("shift_left"))
-        return;
-    if (field.name=="button_top_left") 
-        engine.setValue("[Playlist]","SelectPrevPlaylist",true);
-    if (field.name=="button_bottom_left") 
-        engine.setValue("[Playlist]","SelectNextPlaylist",true);
 }
 
-Xbox360.front_right = function(field) {
-    var controller = Xbox360.controller;
-    if (field.value==controller.buttonStates.released)
-        return;
-    if (!controller.modifiers.get("shift_right")) 
-        return;
-    if (field.name=="button_top_right") {
-        engine.setValue("[Playlist]","SelectPrevTrack",true);
-        var callback = function(field) {
-            engine.setValue("[Playlist]","SelectPrevTrack",true);
-        }
-        controller.setAutoRepeat("hid","button_top_right",callback,150);
-    }
-    if (field.name=="button_bottom_right") {
-        engine.setValue("[Playlist]","SelectNextTrack",true);
-        var callback = function(field) {
-            engine.setValue("[Playlist]","SelectNextTrack",true);
-        }
-        controller.setAutoRepeat("hid","button_bottom_right",callback,150);
-    }
+// resolve triggers
+
+Xbox360.left_volume = function(field) {
+    Xbox360.channel_volume("deck1", field);
 }
+
+Xbox360.right_volume = function(field) {
+    Xbox360.channel_volume("deck2", field);
+}
+
+Xbox360.channel_volume = function(group, field) {
+    var controller = Xbox360.controller;
+    engine_group = controller.resolveGroup(group);
+    if (engine_group==undefined)
+        return;
+    engine.setValue(engine_group,"volume",1.0 - (field.value / 255));
+}
+
+// resolve stick x-axis
+
+Xbox360.left_shuttle = function(field) {
+    Xbox360.shuttle("deck1", field, 1);
+}
+
+Xbox360.right_shuttle = function(field) {
+    Xbox360.shuttle("deck2", field, 1);
+}
+
+// resolve stick y-axis
 
 Xbox360.left_jog = function(field) {
-    var controller = Xbox360.controller;
-    var group = controller.resolveGroup("deck1");
-    if (group==undefined)
-        return;
-    if (field.name=="jog_left_y") {
-        if (field.delta<=0) 
-            return;
-        old_value = engine.getValue(group,"rate");
-        if (field.value<115)
-            engine.setValue(group,"rate",old_value+0.005);
-        else if (field.value>135)
-            engine.setValue(group,"rate",old_value-0.005);
-    }
-    if (field.name=="jog_left_x") {
-
-    }
+    Xbox360.shuttle("deck1", field, -16);
 }
 
 Xbox360.right_jog = function(field) {
+    Xbox360.shuttle("deck2", field, -16);
+}
+
+// shared shuttle code
+
+Xbox360.shuttle = function(group, field, factor) {
     var controller = Xbox360.controller;
-    var group = controller.resolveGroup("deck2");
-    var old_value = undefined;
-    if (group==undefined)
+    engine_group = controller.resolveGroup(group);
+    if (engine_group==undefined)
         return;
-    if (field.name=="jog_right_y") {
-        if (field.delta<=0) 
+    engine.setValue(engine_group,"scratch", (field.value / 32767) * factor);
+}
+
+// generic LED handler
+
+Xbox360.changeLED = function(code) {
+    var controller = Xbox360.controller;
+    var packet = controller.OutputPackets['controller_led'];
+    var field = packet.getField('hid', 'led_ring');
+    field.value = code;
+    packet.send()
+}
+
+
+
+// old jog code, now replaced with a very slow shuttle which is in fact a scratch
+
+Xbox360.jog = function(group, field) {
+    var controller = Xbox360.controller;
+    engine_group = controller.resolveGroup(group);
+    if (engine_group==undefined)
+        return;
+    if ((field.delta <= 0 && field.value > 10) || (field.delta > 0 && field.value < -10)) 
             return;
-        old_value = engine.getValue(group,"rate");
-        if (field.value<115)
-            engine.setValue(group,"rate",old_value+0.005);
-        else if (field.value>135)
-            engine.setValue(group,"rate",old_value-0.005);
-    }
-    if (field.name=="jog_right_x") {
-
-    }
+    engine.setValue(engine_group,"jog", (field.value / 32767) * -16);
 }
 
-Xbox360.left_bend = function(field) {
-    var controller = Xbox360.controller;
-    if (controller.modifiers.get("shift_left")) 
-        return;
-    if (field.name=="pressure_top_left") {
-        Xbox360.jog_bend("deck1","down",field.value);
-        var callback = function(field) {
-            Xbox360.jog_bend("deck1","down",64);
-        }
-        controller.setAutoRepeat("hid","pressure_top_left",callback,50);
-    }
-    if (field.name=="pressure_bottom_left") {
-        Xbox360.jog_bend("deck1","up",field.value);
-        var callback = function() {
-            Xbox360.jog_bend("deck1","up",64);
-        }
-        controller.setAutoRepeat("hid","pressure_bottom_left",callback,50);
-    }
-}
 
-Xbox360.right_bend = function(field) {
-    var controller = Xbox360.controller;
-    if (controller.modifiers.get("shift_right")) 
-        return;
-    if (field.name=="pressure_top_right") {
-        Xbox360.jog_bend("deck2","down",field);
-        var callback = function() {
-            Xbox360.jog_bend("deck2","down",64);
-        }
-        controller.setAutoRepeat("hid","pressure_top_right",callback,50);
-    }
-    if (field.name=="pressure_bottom_right") {
-        Xbox360.jog_bend("deck2","up",field);
-        var callback = function() {
-            Xbox360.jog_bend("deck2","up",64);
-        }
-        controller.setAutoRepeat("hid","pressure_bottom_right",callback,50);
-    }
-}
-
-Xbox360.jog_bend = function(group,direction,value) {
-    var controller = Xbox360.controller;
-    var jog_value = value/32;
-    group = controller.resolveGroup(group);
-    if (group==undefined)
-        return;
-    if (direction=="up")
-        engine.setValue(group,"jog",6);
-    if (direction=="down")
-        engine.setValue(group,"jog",-6);
-}
 
 
